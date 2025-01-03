a9918cd5b5cc0cfe185e67fbaf70350b2b83bf7c
lokesh-katari <lokeshkatari921@outlook.com>
2025-01-03 13:13:33 +0530

    asdf
    Signed-off-by: lokesh-katari <lokeshkatari921@outlook.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/landing/' : '',
-  basePath: isProd ? '/landing' : '',
+  basePath: '/landing' ,
   images: {
     remotePatterns: [
       {
